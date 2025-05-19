6985f4a4ad88211931d5503c28c155b7971c31fd
Tajbir Hossain <mdtajbirhossain031@gmail.com>
2025-05-19 22:31:04 +0600

    sdfsd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayoutWrapper({ children }: { children: ReactNode }) {
     return (
         <html>
             <head>
